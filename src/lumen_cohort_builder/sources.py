42c8acf1bfdbe45dbffdad42bd50058a130d68ce
Andy Maloney <amaloney@mailbox.org>
2025-02-25 13:21:04 -0600

    implementation comments and schema updates

--- src/lumen_cohort_builder/sources.py
+++ src/lumen_cohort_builder/sources.py
@@ -1,4 +1,4 @@
-from typing import override
+from typing import override, Any
 
 import google.auth
 import numpy as np
@@ -29,7 +29,7 @@ class SQLAlchemySource(BaseSQLSource):
         ),
     )
 
-    def __init__(self) -> None:
+    def __init__(self, **params) -> None:
         if self.from_url == "":
             self.url = self.create_url()
         else:
@@ -53,7 +53,7 @@ class SQLAlchemySource(BaseSQLSource):
         password = password if password != PARAM_STR_DEFAULT else None
         host = str(self.host)
         host = host if host != PARAM_STR_DEFAULT else None
-        port = int(self.port)
+        port = int(self.port)  # type: ignore [reportArgumentType]
         port = port if port != PARAM_INT_DEFAULT else None
         database = str(self.database)
         database = database if database != PARAM_STR_DEFAULT else None
@@ -66,7 +66,7 @@ class SQLAlchemySource(BaseSQLSource):
             host=host,
             port=port,
             database=database,
-            query=query,
+            query=query,  # type: ignore [reportArgumentType]
         )
         return url
 
@@ -83,12 +83,15 @@ class BigQuerySource(SQLAlchemySource):
 
     Notes
     -----
-    - A `dataset` in BigQuery is a `table` in SQL.
+    - A `dataset` in BigQuery is similar to a `database` in a traditional relational database.
 
     Attributes
     ----------
-    driver : str, default is `bigquery`
+    tables : list | dict
     dialect : str, default is `bigquery`
+    driver : str, default is `bigquery`
+    project_id : str
+    location : str
     client : Client
         Google BigQuery client object.
 
@@ -98,29 +101,32 @@ class BigQuerySource(SQLAlchemySource):
     location : str
     """
 
-    tables = param.List(doc="List tables, known as datasets in BigQuery.")
+    # NOTE: Can be either a dictionary or a list to allow for "synthetic" tables.
+    tables = param.ClassSelector(
+        class_=(list, dict),
+        doc="Tables, also known as datasets in BigQuery.",
+    )
+    dialect = param.String(default="bigquery", doc="SQL dialect to use.")
 
-    def __init__(self, project_id: str, location: str) -> None:
+    def __init__(self, project_id: str, location: str, **params) -> None:
         self.driver = "bigquery"
-        self.dialect = "bigquery"
         self.project_id = project_id
         self.location = location
         self.client: Client | None = None
 
     def get_tables(self) -> list[str]:
-        # NOTE: The return here prevents calling this method multiple times.
-        if isinstance(self.tables, list):  # type check for param.List
+        # NOTE: The return here prevents this method, which is called multiple times, from
+        #       repopulating the self.tables list.
+        if isinstance(self.tables, dict | list):  # type check for param.[Dict|List]
             if len(self.tables) != 0:
-                return self.tables
+                return list(self.tables)
 
         dataset_ids = []
         if self.client is not None:
-            if not self.tables:  # pyright: ignore [reportGeneralTypeIssues] (param) (param.List)
-                datasets = list(self.client.list_datasets())
-                dataset_ids = [str(dataset.dataset_id) for dataset in datasets]
+            datasets = list(self.client.list_datasets())
+            dataset_ids = [str(dataset.dataset_id) for dataset in datasets]
 
-        self.tables = dataset_ids
-        return self.tables
+        return dataset_ids
 
     def authorize(self) -> None:
         self.credentials, project_id = google.auth.default()
@@ -148,8 +154,8 @@ class BigQuerySource(SQLAlchemySource):
                 datum = {
                     "project": dataset.project,
                     "dataset_id": dataset.dataset_id,
-                    "dataset_description": dataset.description,
                     "dataset_name": dataset.friendly_name,
+                    "dataset_description": dataset.description,
                     "dataset_created": created,
                     "dataset_modified": modified,
                 }
@@ -167,30 +173,42 @@ class BigQuerySource(SQLAlchemySource):
                     "project": project,
                     "dataset_id": dataset_id,
                     "table_id": table.table_id,
-                    "table_description": table.description,
                     "table_name": table.friendly_name,
+                    "table_description": table.description,
                     "table_created": created,
                     "table_modified": modified,
                     "table_num_rows": table.num_rows,
+                    "table_clustering_fields": table.clustering_fields,
+                    "table_labels": table.labels,
                 }
                 data.append(datum)
         return data
 
     def get_column_metadata(
-        self, project: str, dataset_id: str, table_id: str
+        self,
+        project: str,
+        dataset_id: str,
+        table_id: str,
     ) -> list[dict[str, str]]:
         data = []
         if self.client is not None:
             table = self.client.get_table(table=f"{project}.{dataset_id}.{table_id}")
             columns = table.schema
             for column in columns:
+                column_fields = []
+                for column_field in column.fields:
+                    field_datum = column_field.__dict__["_properties"]
+                    # The mode key indicates if the field is nullable or not.
+                    field_datum.pop("mode")
+                    column_fields.append(field_datum)
                 datum = {
                     "project": project,
                     "dataset_id": dataset_id,
                     "table_id": table.table_id,
-                    "table_name": table.friendly_name,
                     "column_id": column.name,
+                    "column_type": column.field_type,
                     "column_description": column.description,
+                    "column_fields": column_fields,
                 }
                 data.append(datum)
         return data
@@ -203,11 +221,14 @@ class ISBSource(BigQuerySource):
         self.project_id = project_id
         self.location = location
         self.authorize()
-        self.client: Client = self.create_client()
-        # self.metadata = self.get_project_metadata()
+        self.client = self.create_client()
+        self.metadata = self.get_project_metadata()
 
     def get_project_metadata(self) -> pd.DataFrame:
         """Get metadata about the ISB-CGC-BQ project.
+
+        The method contains the side-effect of populating the `self.tables` object if it has not
+        already been generated.
 
         Returns
         -------
@@ -257,69 +278,121 @@ class ISBSource(BigQuerySource):
                             "table_description": table.description,
                             "table_created": table_created,
                             "table_modified": table_modified,
-                            "column_name": column.name,
+                            "table_num_rows": table.num_rows,
+                            "table_clustering_fields": table.clustering_fields,
+                            "table_labels": table.labels,
+                            "column_id": column.name,
                             "column_type": column.field_type,
                             "column_description": column.description,
                             "column_fields": column_fields,
                         }
                         data.append(datum)
         df = pd.DataFrame(data)
-        self.tables = sorted(df["dataset_id"].unique())
+        if not self.tables:
+            self.tables = sorted(df["dataset_id"].unique())
         return df
 
     @override
-    def get_schema(self):
-        if self.metadata is None:
-            self.metadata = self.get_project_metadata()
+    def get_schema(
+        self,
+        table: str,
+        limit: int | None = None,
+    ) -> dict[str, dict[str, Any]] | dict[str, Any]:
+        """Determine the schema of the given `table`.
 
+        This method overrides the inherited `get_schema` from the base class `Source`. The reason
+        why we override
+
+        Parameters
+        ----------
+        table : str
+            The name of the table. Must be in the form: f"{project}.{dataset_id}.{table_id}".
+
+        Returns
+        -------
+        dict[str, dict[str, Any]] | dict[str, Any]
+        """
         schema = {}
-        for row_index, row in self.metadata.iterrows():
-            project = row["project"]
-            dataset_id = row["dataset_id"]
-            table_id = row["table_id"]
+        if self.client is not None:
+            bq_table = self.client.get_table(table=table)
+            project = bq_table.project
+            dataset_id = bq_table.dataset_id
+            table_id = bq_table.table_id
+            table_name = f"{project}.{dataset_id}.{table_id}"
 
-            dataset_name = f"{project}.{dataset_id}.{table_id}"
-            if dataset_name not in schema:
-                schema[dataset_name] = {}
+            columns = bq_table.schema
+            for column in columns:
+                column_name = column.name
+                column_type = column.field_type
 
-            column_name = row["column_name"]
-            column_type = str(row["column_type"])
+                if table_name not in schema:
+                    schema[table_name] = {}
 
-            if column_type == "STRING":
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {"type": column_type.lower(), "enum": []}
+                if column_type == "STRING":
+                    if column_name not in schema[table_name]:
+                        schema[table_name][column_name] = {"type": column_type.lower(), "enum": []}
 
-            elif column_type == "TIMESTAMP":
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {"type": column_type.lower()}
+                    query = (
+                        f"SELECT {column_name} "
+                        f"FROM {project}.{dataset_id}.{table_id} "
+                        f"GROUP BY {column_name};"
+                    )
+                    job = self.client.query(query)
+                    results = sorted(result[0] for result in job.result() if result[0] is not None)
+                    schema[table_name][column_name]["enum"] = results
 
-            elif column_type == "DATE":
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {"type": column_type.lower()}
+                elif column_type in ["TIMESTAMP", "DATE"]:
+                    if column_name not in schema[table_name]:
+                        schema[table_name][column_name] = {
+                            "type": "str",
+                            "format": "datetime",
+                            "min": "",
+                            "max": "",
+                        }
 
-            elif column_type == "BOOLEAN":
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {"type": column_type.lower()}
+                    query = (
+                        f"SELECT MIN({column_name}) AS min_date, MAX({column_name}) AS max_date "
+                        f"FROM {project}.{dataset_id}.{table_id};"
+                    )
+                    job = self.client.query(query)
+                    results = list(job.result())
 
-            elif column_type == "RECORD":
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {"type": "items"}
+                    min_date = results[0][0]
+                    if min_date.tz:
+                        min_date = min_date.astimezone("UTC").tz_localize(None)
+                    min_date_str = pd.to_datetime(min_date).strftime("%Y-%m-%dT%H:%M:%S.%f")
+                    schema[table_name][column_name]["min"] = min_date_str
 
-            elif column_type in ["FLOAT", "INTEGER"]:
-                if column_name not in schema[dataset_name]:
-                    schema[dataset_name][column_name] = {
-                        "type": column_type.lower() if column_type == "INTEGER" else "number",
-                        "inclusiveMinimum": np.nan,
-                        "inclusiveMaximum": np.inf,
-                    }
+                    max_date = results[0][1]
+                    if max_date.tz:
+                        max_date = max_date.astimezone("UTC").tz_localize(None)
+                    max_date_str = pd.to_datetime(max_date).strftime("%Y-%m-%dT%H:%M:%S.%f")
+                    schema[table_name][column_name]["max"] = max_date_str
 
-                # We will query for the min/max values in the column.
-                query = (
-                    f"SELECT APPROX_QUANTILES({column_name, 4}) AS bounds "
-                    f"FROM `{project}.{dataset_id}.{table_id}`"
-                )
-                job = self.client.query(query)
-                results = dict(job.result())
-                inclusiveMinimum, inclusiveMaximum = results["bounds"]
-                schema[dataset_name][column_name]["inclusiveMinimum"] = inclusiveMinimum
-                schema[dataset_name][column_name]["inclusiveMaximum"] = inclusiveMaximum
+                elif column_type == "BOOLEAN":
+                    if column_name not in schema[table_name]:
+                        schema[table_name][column_name] = {"type": column_type.lower()}
+
+                elif column_type == "RECORD":
+                    # FIXME: Ignore nested data structures for now.
+                    continue
+
+                elif column_type in ["FLOAT", "INTEGER"]:
+                    if column_name not in schema[table_name]:
+                        schema[table_name][column_name] = {
+                            "type": column_type.lower() if column_type == "INTEGER" else "number",
+                            "inclusiveMinimum": -1.0 * np.inf,
+                            "inclusiveMaximum": np.inf,
+                        }
+
+                    # We will query for the min/max values in the column.
+                    query = (
+                        f"SELECT APPROX_QUANTILES({column_name, 1}) AS bounds "
+                        f"FROM {project}.{dataset_id}.{table_id}"
+                    )
+                    job = self.client.query(query)
+                    results = dict(job.result())
+                    inclusiveMinimum, inclusiveMaximum = results["bounds"]
+                    schema[table_name][column_name]["inclusiveMinimum"] = inclusiveMinimum
+                    schema[table_name][column_name]["inclusiveMaximum"] = inclusiveMaximum
+        return schema
