2ccddb2999b71339d5ac009c86633dd4163f6674
Andy Maloney <amaloney@mailbox.org>
2025-03-03 13:07:43 -0600

    resolves #11

--- src/lumen_cohort_builder/main.py
+++ src/lumen_cohort_builder/main.py
@@ -1,18 +1,10 @@
-import os
-
 import lumen.ai as lmai
-# from lumen.sources.duckdb import DuckDBSource
 
 from lumen_cohort_builder.sources import ISBSource
 
 
-PROJECT_ID = "isb-cgc-bq"
-os.environ["GOOGLE_CLOUD_PROJECT"] = PROJECT_ID
 app = lmai.ExplorerUI(
-    # data=["./data/penguins.csv"],
-    data=[ISBSource(project_id=PROJECT_ID, location="us")],
-    # data=DuckDBSource(tables=["./data/penguins.parquet"]),
+    data=[ISBSource()],
     llm=lmai.llm.LlamaCpp(),
-    # agents=[],
 )
 app.servable()
